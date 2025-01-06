371c02d66162ea290f361c9a18b4b028e6bee8cb
Andreas Huber <andreashuber69@users.noreply.github.com>
2025-01-06 20:53:32 +0100

    fix: filename

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,4 @@ import { ESLint } from "eslint";
 
 const eslint = new ESLint({ flags: ["unstable_ts_config"] });
 // eslint-disable-next-line no-console
-console.log(await eslint.calculateConfigForFile("src/languageOptions.js"));
+console.log(await eslint.calculateConfigForFile("src/index.ts"));
