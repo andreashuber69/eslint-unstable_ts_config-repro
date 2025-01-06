0a146fefaed06f26ae0e48f33edda965eee928f7
Andreas Huber <andreashuber69@users.noreply.github.com>
2025-01-06 20:40:14 +0100

    refactor: inline languageOptions

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,26 @@
 import { fixupPluginRules } from "@eslint/compat";
+import globals from "globals";
 import importPlugin from "eslint-plugin-import";
-import { languageOptions } from "./src/languageOptions.js";
 import pluginJs from "@eslint/js";
+import tsParser from "@typescript-eslint/parser"
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
   pluginJs.configs.all,
   tseslint.configs.all,
   {
-    languageOptions,
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+      parser: tsParser,
+      parserOptions: {
+        projectService: {
+          allowDefaultProject: ["eslint.config.js"]
+        },
+        tsconfigRootDir: process.cwd(),
+      },
+    },
     plugins: {
       import: fixupPluginRules(importPlugin),
     },
