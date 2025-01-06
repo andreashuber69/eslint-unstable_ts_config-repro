35d064a86120767cbb7308094a1d05e307772c1b
Andreas Huber <andreashuber69@users.noreply.github.com>
2025-01-06 17:38:18 +0100

    feat: enable typed linting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,24 @@
+import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
+import tsParser from "@typescript-eslint/parser"
 
 /** @type {import('eslint').Linter.Config[]} */
 export default tseslint.config(
   pluginJs.configs.recommended,
   tseslint.configs.recommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+      parser: tsParser,
+      parserOptions: {
+        projectService: {
+          allowDefaultProject: ["eslint.config.mjs"]
+        },
+        tsconfigRootDir: process.cwd(),
+      },
+    },
+  }
 );
