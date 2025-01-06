cdf114ca31f0e399fafefcac3dffa33fe22600d7
Andreas Huber <andreashuber69@users.noreply.github.com>
2025-01-06 17:00:59 +0100

    chore: initial setup

---
+++ eslint.config.mjs
@@ -0,0 +1,17 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default tseslint.config(
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"]
+  },
+  {
+    languageOptions: {
+      globals: globals.node
+    }
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+);
